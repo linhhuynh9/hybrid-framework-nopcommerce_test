01595aa818b199631ea78c39336bfe98358ac349
Huynh Thi Truc Linh <Huy@DESKTOP-8PN57LU>
2022-11-14 10:52:50 +0700

    Create new automation test framework

---
+++ actions/commons/GlobaConstant.java
@@ -0,0 +1,6 @@
+package commons;
+
+public class GlobaConstant {
+	// Infor dùng cho toàn bộ tất cả các class khác trong Frw có thể truy cập được
+	// Chia sẻ những biến dùng chung. VD như User/pass => App/Database...
+}
